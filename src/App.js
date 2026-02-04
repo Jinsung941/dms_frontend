da92e2e65bc144b45ffa728897eb0d695c49a4b2
cm030627 <cm030627@gmail.com>
2026-02-04 21:25:30 +0900

    머지연습

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-
 import Chat from "./Chat";
+import Home from "./page/Home";
+import Archived from "./page/Archived";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <Chat/>
+    <Routes>
+      <Route path="/" element= {<Home />} />
+      <Route path="/archived" element={<Archived />} />
+    </Routes>
   );
 }
 
